9653884ad7ad4ec6c5bc9935ed782abdf0fcc015
hunterjr88 <dsanders_m633@outlook.com>
2018-10-30 09:55:05 -0400

    initial commit

---
+++ Game.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPop
+{
+    public static class Game
+    {
+        
+        public static void Directions(
+            string Input1, 
+            string Input2, 
+            string Input3, 
+            string Input4, 
+            string Input5, 
+            string Input6)
+
+        {
+              
+        string FirstNumber = Input1;
+        string SecondNumber = Input2;
+        string ThirdNumber = Input3;
+
+        string FirstWord = Input4;
+        string SecondWord = Input5;
+        string ThirdWord = Input6;
+            
+            
+            Console.WriteLine("\nGame Rules are now...");
+            Console.WriteLine($"\n{FirstNumber} = {FirstWord}");
+            Console.WriteLine($"{SecondNumber} = {SecondWord}");
+            Console.WriteLine($"{ThirdNumber} = {ThirdWord}");
+
+            Console.WriteLine($"{FirstNumber} and {SecondNumber} = {FirstWord} {SecondWord}");
+            Console.WriteLine($"{FirstNumber} and {ThirdNumber} = {FirstWord} {ThirdWord}");
+            Console.WriteLine($"{SecondNumber} and {ThirdNumber} = {SecondWord} {ThirdWord}");
+            Console.WriteLine($"{FirstNumber},{SecondNumber}, and {ThirdNumber} = {FirstWord} {SecondWord} {ThirdWord}");
+
+        }
+        public static void Substitutions()
+        {
+            
+            //Subsituted Multiples
+            Console.WriteLine("\nWhat should the first multiple be?");
+            string FirstNumber = Console.ReadLine().ToLower();
+
+            Console.WriteLine("\nWhat should the second multiple be?");
+            string SecondNumber = Console.ReadLine().ToLower();
+
+            Console.WriteLine("\nWhat should the third multiple be?");
+            string ThirdNumber = Console.ReadLine().ToLower();
+
+            //Substituted Words
+            Console.WriteLine("\nWhat should the first word be?");
+            string FirstWord = Console.ReadLine().ToLower();
+
+            Console.WriteLine("\nWhat should the second word be?");
+            string SecondWord = Console.ReadLine().ToLower();
+
+            Console.WriteLine("\nWhat should the third word be?");
+            string ThirdWord = Console.ReadLine().ToLower();
+
+            Console.WriteLine("\nThe New Game Rules are...");
+            Console.WriteLine($"\n{FirstNumber} = {FirstWord}");
+            Console.WriteLine($"{SecondNumber} = {SecondWord}");
+            Console.WriteLine($"{ThirdNumber} = {ThirdWord}");
+
+            Console.WriteLine($"{FirstNumber} and {SecondNumber} = {FirstWord} {SecondWord}");
+            Console.WriteLine($"{FirstNumber} and {ThirdNumber} = {FirstWord} {ThirdWord}");
+            Console.WriteLine($"{SecondNumber} and {ThirdNumber} = {SecondWord} {ThirdWord}");
+            Console.WriteLine($"{FirstNumber},{SecondNumber}, and {ThirdNumber} = {FirstWord} {SecondWord} {ThirdWord}");
+        }
+
+            public static void Play(
+                    string Input1,
+                    string Input2,
+                    string Input3,
+                    string Input4,
+                    string Input5,
+                    string Input6,
+                    string PlayerInput
+                )  
+            {
+            
+            try
+            {
+                int FirstNumber = Int32.Parse(Input1);
+                int SecondNumber = Int32.Parse(Input2);
+                int ThirdNumber = Int32.Parse(Input3);
+
+                string FirstWord = Input4;
+                string SecondWord = Input5;
+                string ThirdWord = Input6;
+                int x = Int32.Parse(PlayerInput);
+
+                
+                if (x % FirstNumber == 0 && x % SecondNumber != 0 && x % ThirdNumber != 0)//Multiples of three return Fizz
+                {
+                    Console.WriteLine(FirstWord);
+                }
+                else if (x % FirstNumber != 0 && x % SecondNumber == 0 && x % ThirdNumber != 0)//Multiples of five return Buzz
+                {
+                    Console.WriteLine(SecondWord);
+                }
+                else if (x % FirstNumber != 0 && x % SecondNumber != 0 && x % ThirdNumber == 0)//Multiples of seven return Pop
+                {
+                    Console.WriteLine(ThirdWord);
+                }
+                else if (x % FirstNumber == 0 && x % SecondNumber == 0 && x % ThirdNumber != 0)//Multiples of three and five return Fizz Buzz
+                    {
+                    Console.WriteLine(FirstWord + ' ' + SecondWord);
+                }
+                else if (x % FirstNumber == 0 && x % SecondNumber != 0 && x % ThirdNumber == 0)//Multiples of Three and Seven Return Fizz Pop
+                {
+                    Console.WriteLine(FirstWord + ' ' + ThirdWord);
+                }
+                else if(x % FirstNumber != 0 && x % SecondNumber == 0 && x % ThirdNumber == 0)//Multiples of Five and Seven Return Buzz Pop
+                {
+                    Console.WriteLine(SecondWord + ' ' + ThirdWord);
+                }
+                else if(x % FirstNumber == 0 && x % SecondNumber == 0 && x % ThirdNumber == 0)//Multiples of Three, Five and Seven Return Fizz Buzz Pop
+                {
+                    Console.WriteLine(FirstWord + ' ' + SecondWord + ' ' + ThirdWord);
+                }
+                else//Normal numbers return themselves
+                {
+                    Console.WriteLine(x);
+                }
+                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Not an Applicable Input");
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to Fizz Buzz Pop the game!!");
+
+            Console.WriteLine("\nWith your host David Sanders (Srum Master Level 5)");
+
+            //Player Input
+            Console.WriteLine("\nFirst things first.  Please tell me your name?");
+
+            var user = Console.ReadLine();
+
+            Console.WriteLine($"\nWelcome {user}");
+
+            //Defaults
+            string FirstNumber = "3";
+            string SecondNumber = "5";
+            string ThirdNumber = "7";
+
+            string FirstWord = "FIZZ";
+            string SecondWord = "BUZZ";
+            string ThirdWord = "POP";
+
+            Game.Directions(
+                FirstNumber, 
+                SecondNumber, 
+                ThirdNumber, 
+                FirstWord, 
+                SecondWord, 
+                ThirdWord
+                );
+
+            Console.WriteLine($"\n{user} would you like to customize the game and add your own words and multiples? yes/no");
+
+            string answer = Console.ReadLine().ToLower();
+
+            if(answer == "yes")
+            {
+
+                //Substituted Multiples
+                Console.WriteLine("\nWhat should the first multiple be?");
+                FirstNumber = Console.ReadLine().ToLower();
+
+                Console.WriteLine("\nWhat should the second multiple be?");
+                SecondNumber = Console.ReadLine().ToLower();
+
+                Console.WriteLine("\nWhat should the third multiple be?");
+                ThirdNumber = Console.ReadLine().ToLower();
+
+                //Substituted Words
+                Console.WriteLine("\nWhat should the first word be?");
+                FirstWord = Console.ReadLine().ToLower();
+
+                Console.WriteLine("\nWhat should the second word be?");
+                SecondWord = Console.ReadLine().ToLower();
+
+                Console.WriteLine("\nWhat should the third word be?");
+                ThirdWord = Console.ReadLine().ToLower();
+
+
+                //Display new directions
+                Game.Directions(
+                    FirstNumber,
+                    SecondNumber,
+                    ThirdNumber,
+                    FirstWord,
+                    SecondWord,
+                    ThirdWord
+                );
+            }
+            else if(answer == "no")
+            {
+                Console.WriteLine("\nIf it aint broken, dont fix it.  I like that!");
+            }
+            else
+            {
+                Console.WriteLine("\nI will take that as a no....");
+            }
+
+
+            //Start Game
+            Console.WriteLine("\nAlright then, let's get started");
+            var Input = "playing";
+
+            do
+            {
+                Console.WriteLine("\nType below. (Done?, type 'end game')");
+                Input = Console.ReadLine().ToLower();
+                if(Input != "end game")
+                {
+                   Game.Play(
+                        FirstNumber,
+                        SecondNumber,
+                        ThirdNumber,
+                        FirstWord,
+                        SecondWord,
+                        ThirdWord,
+                        Input
+                    );
+                }
+            }
+            while (Input != "end game");
+
+            Console.Write("\nGame Over!");
+            Console.Write("\nHave a great day!! Good Bye!!");
+            Console.ReadKey(true);
+        }
+    }
+}
